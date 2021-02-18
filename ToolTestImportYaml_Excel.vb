e0d9a88c790638e14b790ac740896468416a7d08
Jeremy D. Gerdes <seakintruth@gmail.com>
2021-02-18 15:19:23 -0500

    An example for use in an excel file

---
+++ ToolTestImportYaml_Excel.vb
@@ -0,0 +1,18 @@
+
+Public Sub ToolTestImportYaml(strYamlFilePath As String, rngDestination As Range)
+Dim arryYaml As Variant
+Dim rngDestinationReturn As Range
+    arryYaml = ParseYAMLtoArray(strYamlFilePath)
+    rngDestination.Worksheet.Activate
+    rngDestination.Activate
+    Set rngDestinationReturn = rngDestination.Worksheet.Range( _
+        rngDestination.Address, _
+        rngDestination.Offset( _
+            UBound(arryYaml, 1) - LBound(arryYaml, 1), _
+            UBound(arryYaml, 2) - LBound(arryYaml, 2) _
+        ).Address _
+    )
+    'Assign the values to the destination range
+    rngDestinationReturn.Value = arryYaml
+End Sub
+
